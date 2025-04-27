491efccf862972136bde2ec2be39399e72ba8c68
Renne Augusto <renne.augusto@gmail.com>
2025-04-27 14:51:09 -0300

    deploy no vercel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Todo obstáculo é destruído pelo rigor. by DaVinci</h1>;
+    return <h1>Todo obstáculo é destruído pelo rigor. by DaVinci. Obrigado por fazer parte da minha história amore!! ❤️❤️</h1>;
   }
   
   export default Home;
